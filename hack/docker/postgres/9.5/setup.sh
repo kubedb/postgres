0ccdbea4601264c7b70d1af58ac7db62e0245261
aerokite <aerofoil.kite@gmail.com>
2017-04-07 16:27:29 +0600

    fixed

--- hack/docker/postgres/9.5/setup.sh
+++ hack/docker/postgres/9.5/setup.sh
@@ -26,40 +26,40 @@ fi
 
 build() {
     pushd $REPO_ROOT/hack/docker/postgres/9.5
-	for name in "${docker_names[@]}"
-	do
-		cd $name
-		docker build -t appscode/$IMG:$TAG-$name .
-		cd ..
-	done
+    for name in "${docker_names[@]}"
+    do
+        cd $name
+        docker build -t appscode/$IMG:$TAG-$name .
+        cd ..
+    done
 	popd
 }
 
 docker_push() {
-	for name in "${docker_names[@]}"
-	do
+    for name in "${docker_names[@]}"
+    do
         docker_up $IMG:$TAG-$name
-	done
+    done
 }
 
 docker_release() {
-	for name in "${docker_names[@]}"
-	do
+    for name in "${docker_names[@]}"
+    do
         docker push appscode/$IMG:$TAG-$name
-	done
+    done
 }
 
 docker_check() {
-	for i in "${docker_names[@]}"
-	do
-		echo "Chcking $IMG ..."
-		name=$i-$(date +%s | sha256sum | base64 | head -c 8 ; echo)
-		docker run -d -P -it --name=$name appscode/$IMG:$TAG-$i
-		docker exec -it $name ps aux
-		sleep 5
-		docker exec -it $name ps aux
-		docker stop $name && docker rm $name
-	done
+    for i in "${docker_names[@]}"
+    do
+        echo "Chcking $IMG ..."
+        name=$i-$(date +%s | sha256sum | base64 | head -c 8 ; echo)
+        docker run -d -P -it --name=$name appscode/$IMG:$TAG-$i
+        docker exec -it $name ps aux
+        sleep 5
+        docker exec -it $name ps aux
+        docker stop $name && docker rm $name
+    done
 }
 
 binary_repo $@
