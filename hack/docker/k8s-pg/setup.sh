53186442894fa4e5fa890a79d573eb04ccac0777
tamal <tamal@appscode.com>
2017-04-20 11:29:53 -0700

    added-all

--- hack/docker/k8s-pg/setup.sh
+++ hack/docker/k8s-pg/setup.sh
@@ -10,8 +10,7 @@ BIN=$GOPATH/bin
 ROOT=$GOPATH
 REPO_ROOT=$GOPATH/src/github.com/k8sdb/postgres
 
-source "$REPO_ROOT/hack/libbuild/common/lib.sh"
-source "$REPO_ROOT/hack/libbuild/common/public_image.sh"
+source "$REPO_ROOT/hack/libbuild/common/k8sdb_image.sh"
 
 APPSCODE_ENV=${APPSCODE_ENV:-dev}
 IMG=k8s-pg
@@ -49,7 +48,7 @@ COPY k8s-pg /k8s-pg
 USER nobody:nobody
 ENTRYPOINT ["/k8s-pg"]
 EOL
-    local cmd="docker build -t appscode/$IMG:$TAG ."
+    local cmd="docker build -t k8sdb/$IMG:$TAG ."
     echo $cmd; $cmd
 
     rm k8s-pg Dockerfile
@@ -66,10 +65,7 @@ docker_push() {
         echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
         exit 0
     fi
-
-    if [[ "$(docker images -q appscode/$IMG:$TAG 2> /dev/null)" != "" ]]; then
-        docker_up $IMG:$TAG
-    fi
+    hub_canary
 }
 
 docker_release() {
@@ -81,10 +77,7 @@ docker_release() {
         echo "'apply_tag' to release binaries and/or docker images."
         exit 1
     fi
-
-    if [[ "$(docker images -q appscode/$IMG:$TAG 2> /dev/null)" != "" ]]; then
-        docker push appscode/$IMG:$TAG
-    fi
+    hub_up
 }
 
 source_repo $@
