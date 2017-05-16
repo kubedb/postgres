828b3c63596c5fe2f507d633d5b1c866eded8823
shahriar <aerofoil.kite@gmail.com>
2017-05-16 11:20:37 +0600

    Add examples to use Storage and BackupSchedule

--- example/statefulset/run.sh
+++ example/statefulset/run.sh
@@ -2,8 +2,4 @@
 
 REPO_ROOT=$GOPATH/src/github.com/k8sdb/postgres
 example=$REPO_ROOT/example/statefulset
-
-kubectl create -f $example/postgres.yaml
-kubectl create -f $example/service.yaml
-kubectl create -f $example/secret.yaml
-kubectl create -f $example/service-account.yaml
+kubectl create -f $example
