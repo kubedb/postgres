8a89ddccb7e8f4302e85705ee622cd088671b8d9
tamal <tamal@appscode.com>
2017-05-26 06:17:41 -0700

    added-all

--- run.go
+++ run.go
@@ -74,10 +74,10 @@ func NewCmdRun() *cobra.Command {
 	}
 	cmd.Flags().StringVar(&masterURL, "master", masterURL, "The address of the Kubernetes API server (overrides any value in kubeconfig)")
 	cmd.Flags().StringVar(&kubeconfigPath, "kubeconfig", kubeconfigPath, "Path to kubeconfig file with authorization information (the master location is set by the master flag).")
-	cmd.Flags().StringVar(&opt.PostgresUtilTag, "postgres-util", opt.PostgresUtilTag, "Tag of postgres util")
-	cmd.Flags().StringVar(&opt.ExporterNamespace, "exporter-ns", opt.ExporterNamespace, "Namespace for monitoring exporter")
-	cmd.Flags().StringVar(&opt.ExporterTag, "exporter", opt.ExporterTag, "Tag of monitoring expoter")
 	cmd.Flags().StringVar(&opt.GoverningService, "governing-service", opt.GoverningService, "Governing service for database statefulset")
+	cmd.Flags().StringVar(&opt.PostgresUtilTag, "postgres.util-tag", opt.PostgresUtilTag, "Tag of postgres util")
+	cmd.Flags().StringVar(&opt.ExporterNamespace, "exporter.namespace", opt.ExporterNamespace, "Namespace for monitoring exporter")
+	cmd.Flags().StringVar(&opt.ExporterTag, "exporter.tag", opt.ExporterTag, "Tag of monitoring exporter")
 	cmd.Flags().StringVar(&opt.Address, "address", opt.Address, "Address to listen on for web interface and telemetry.")
 	return cmd
 }
