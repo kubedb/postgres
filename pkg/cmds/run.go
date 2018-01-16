bad7b8c80bd0afcfb25060bb871157bc7a239a9b
shahriar <aerofoil.kite@gmail.com>
2018-01-16 13:10:38 +0600

    fixed

--- pkg/cmds/run.go
+++ pkg/cmds/run.go
@@ -21,12 +21,15 @@ import (
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"flag"
 )
 
 func NewCmdRun(version string) *cobra.Command {
 	var (
-		masterURL      string
-		kubeconfigPath string
+		masterURL          string
+		kubeconfigPath     string
+		prometheusCrdGroup = pcm.Group
+		prometheusCrdKinds = pcm.DefaultCrdKinds
 	)
 
 	opt := controller.Options{
@@ -54,7 +57,7 @@ func NewCmdRun(version string) *cobra.Command {
 			client := kubernetes.NewForConfigOrDie(config)
 			apiExtKubeClient := crd_cs.NewForConfigOrDie(config)
 			extClient := cs.NewForConfigOrDie(config)
-			promClient, err := pcm.NewForConfig(config)
+			promClient, err := pcm.NewForConfig(&prometheusCrdKinds, prometheusCrdGroup, config)
 			if err != nil {
 				log.Fatalln(err)
 			}
@@ -97,6 +100,11 @@ func NewCmdRun(version string) *cobra.Command {
 	cmd.Flags().StringVar(&opt.Docker.ExporterTag, "exporter-tag", opt.Docker.ExporterTag, "Tag of kubedb/operator used as exporter")
 	cmd.Flags().StringVar(&opt.Address, "address", opt.Address, "Address to listen on for web interface and telemetry.")
 	cmd.Flags().BoolVar(&opt.EnableRbac, "rbac", opt.EnableRbac, "Enable RBAC for database workloads")
+
+	flagset := flag.CommandLine
+	flagset.StringVar(&prometheusCrdGroup, "prometheus-crd-apigroup", prometheusCrdGroup, "prometheus CRD  API group name")
+	flagset.Var(&prometheusCrdKinds, "prometheus-crd-kinds", " - EXPERIMENTAL (could be removed in future releases) - customize CRD kind names")
+	cmd.Flags().AddGoFlagSet(flagset)
 
 	return cmd
 }
