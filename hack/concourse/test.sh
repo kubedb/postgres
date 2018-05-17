7870e8213c066d67adbfbd9d8dbe4e49ad68df8f
tahsinrahman <tahsynx@gmail.com>
2018-05-17 21:02:02 +0600

    update pipeline; build and push docker image before creating cluster;

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -7,21 +7,21 @@ entrypoint.sh
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker run hello-world
 
-#install python pip
+# install python pip
 apt-get update > /dev/null
 apt-get install -y python python-pip > /dev/null
 
-#install kubectl
+# install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &> /dev/null
 chmod +x ./kubectl
 mv ./kubectl /bin/kubectl
 
-#install onessl
+# install onessl
 curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 \
   && chmod +x onessl \
   && mv onessl /usr/local/bin/
 
-#install pharmer
+# install pharmer
 pushd /tmp
 curl -LO https://cdn.appscode.com/binaries/pharmer/0.1.0-rc.3/pharmer-linux-amd64
 chmod +x pharmer-linux-amd64
@@ -44,31 +44,49 @@ function cleanup {
 }
 trap cleanup EXIT
 
+
+# copy postgres to $GOPATH
+mkdir -p $GOPATH/src/github.com/kubedb
+cp -r postgres $GOPATH/src/github.com/kubedb
+pushd $GOPATH/src/github.com/kubedb/postgres
+
 # name of the cluster
 # nameing is based on repo+commit_hash
 pushd postgres
 NAME=postgres-$(git rev-parse --short HEAD)
+
+./hack/builddeps.sh
+export APPSCODE_ENV=dev
+export DOCKER_REGISTRY=kubedbci
+./hack/docker/pg-operator/make.sh build
+./hack/docker/pg-operator/make.sh push
+./hack/docker/postgres/9.6.7/make.sh build
+./hack/docker/postgres/9.6.7/make.sh push
+./hack/docker/postgres/9.6/make.sh
+./hack/docker/postgres/10.2/make.sh build
+./hack/docker/postgres/10.2/make.sh push
+
 popd
 
-#create credential file for pharmer
+# create credential file for pharmer
 cat > cred.json <<EOF
 {
         "token" : "$TOKEN"
 }
 EOF
 
-#create cluster using pharmer
-#note: make sure the zone supports volumes, not all regions support that
-#"We're sorry! Volumes are not available for Droplets on legacy hardware in the NYC3 region"
+# create cluster using pharmer
+# note: make sure the zone supports volumes, not all regions support that
+# "We're sorry! Volumes are not available for Droplets on legacy hardware in the NYC3 region"
 pharmer create credential --from-file=cred.json --provider=DigitalOcean cred
 pharmer create cluster $NAME --provider=digitalocean --zone=nyc1 --nodes=2gb=1 --credential-uid=cred --kubernetes-version=v1.10.0
 pharmer apply $NAME
 pharmer use cluster $NAME
-#wait for cluster to be ready
+# wait for cluster to be ready
 sleep 300
 kubectl get nodes
 
-#create storageclass
+# create storageclass
 cat > sc.yaml <<EOF
 apiVersion: storage.k8s.io/v1
 kind: StorageClass
@@ -79,16 +97,13 @@ parameters:
 provisioner: external/pharmer
 EOF
 
-#create storage-class
+# create storage-class
 kubectl create -f sc.yaml
 sleep 120
 kubectl get storageclass
 
 export CRED_DIR=$(pwd)/creds/gcs/gcs.json
 
-#copy postgres to $GOPATH
-mkdir -p $GOPATH/src/github.com/kubedb
-cp -r postgres $GOPATH/src/github.com/kubedb
 pushd $GOPATH/src/github.com/kubedb/postgres
 
 # create config/.env file that have all necessary creds
@@ -117,15 +132,5 @@ EOF
 
 
 # run tests
-./hack/builddeps.sh
-export APPSCODE_ENV=dev
-export DOCKER_REGISTRY=kubedbci
-./hack/docker/pg-operator/make.sh build
-./hack/docker/pg-operator/make.sh push
-./hack/docker/postgres/9.6.7/make.sh build
-./hack/docker/postgres/9.6.7/make.sh push
-./hack/docker/postgres/9.6/make.sh
-./hack/docker/postgres/10.2/make.sh build
-./hack/docker/postgres/10.2/make.sh push
 source ./hack/deploy/setup.sh --docker-registry=kubedbci
 ./hack/make.py test e2e --v=1 --storageclass=standard --selfhosted-operator=true
