81366712aa8f6427463d2f837337ec66fb962279
the-redback <maruf.2hin@gmail.com>
2019-02-04 11:59:45 +0600

    revendored

--- vendor/github.com/kubedb/apimachinery/apis/kubedb/v1alpha1/openapi_generated.go
+++ vendor/github.com/kubedb/apimachinery/apis/kubedb/v1alpha1/openapi_generated.go
@@ -1343,6 +1343,7 @@ func schema_apimachinery_apis_kubedb_v1alpha1_LeaderElectionConfig(ref common.Re
 						},
 					},
 				},
+				Required: []string{"leaseDurationSeconds", "renewDeadlineSeconds", "retryPeriodSeconds"},
 			},
 		},
 		Dependencies: []string{},
@@ -2471,7 +2472,7 @@ func schema_apimachinery_apis_kubedb_v1alpha1_PostgresSpec(ref common.ReferenceC
 					},
 					"leaderElection": {
 						SchemaProps: spec.SchemaProps{
-							Description: "Leader Election configs",
+							Description: "Leader election configuration",
 							Ref:         ref("github.com/kubedb/apimachinery/apis/kubedb/v1alpha1.LeaderElectionConfig"),
 						},
 					},
@@ -3566,6 +3567,13 @@ func schema_apimachinery_apis_kubedb_v1alpha1_SnapshotSpec(ref common.ReferenceC
 							Ref: ref("kmodules.xyz/objectstore-api/api/v1.B2Spec"),
 						},
 					},
+					"storageType": {
+						SchemaProps: spec.SchemaProps{
+							Description: "StorageType can be durable or ephemeral. If not given, database storage type will be used.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
 					"podTemplate": {
 						SchemaProps: spec.SchemaProps{
 							Description: "PodTemplate is an optional configuration for pods used to take database snapshots",
@@ -3574,7 +3582,7 @@ func schema_apimachinery_apis_kubedb_v1alpha1_SnapshotSpec(ref common.ReferenceC
 					},
 					"podVolumeClaimSpec": {
 						SchemaProps: spec.SchemaProps{
-							Description: "PodVolumeClaimSpec is used to specify temporary storage for backup/restore Job",
+							Description: "PodVolumeClaimSpec is used to specify temporary storage for backup/restore Job. If not given, database's PvcSpec will be used. If storageType is durable, then a PVC will created using this PVCSpec. If storageType is ephemeral, then an empty directory will be created of size PvcSpec.Resources.Requests[core.ResourceStorage].",
 							Ref:         ref("k8s.io/api/core/v1.PersistentVolumeClaimSpec"),
 						},
 					},
