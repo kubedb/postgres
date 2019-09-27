533443603e92c377ab167c75be69ff2a8177830b
Tamal Saha <tamal@appscode.com>
2019-09-26 19:29:13 -0700

    Run e2e tests using GitHub actions
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -2,6 +2,7 @@ package e2e_test
 
 import (
 	"flag"
+	"os"
 	"path/filepath"
 	"testing"
 	"time"
@@ -14,10 +15,10 @@ import (
 	"k8s.io/client-go/kubernetes"
 	clientSetScheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	ka "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset"
 	"kmodules.xyz/client-go/logs"
+	"kmodules.xyz/client-go/tools/clientcmd"
 	appcat_cs "kmodules.xyz/custom-resources/client/clientset/versioned/typed/appcatalog/v1alpha1"
 	cs "kubedb.dev/apimachinery/client/clientset/versioned"
 	"kubedb.dev/apimachinery/client/clientset/versioned/scheme"
@@ -33,12 +34,22 @@ import (
 // 2. ./hack/make.py test e2e --v=1  --docker-registry=kubedbci --db-catalog=10.2-v1 --db-version=10.2-v2 --db-tools=10.2-v2 --selfhosted-operator=true
 
 var (
-	storageClass = "standard"
+	storageClass   = "standard"
+	kubeconfigPath = func() string {
+		kubecfg := os.Getenv("KUBECONFIG")
+		if kubecfg != "" {
+			return kubecfg
+		}
+		return filepath.Join(homedir.HomeDir(), ".kube", "config")
+	}()
+	kubeContext = ""
 )
 
 func init() {
 	scheme.AddToScheme(clientSetScheme.Scheme)
 
+	flag.StringVar(&kubeconfigPath, "kubeconfig", kubeconfigPath, "Path to kubeconfig file with authorization information (the master location is set by the master flag).")
+	flag.StringVar(&kubeContext, "kube-context", "", "Name of kube context")
 	flag.StringVar(&storageClass, "storageclass", storageClass, "Kubernetes StorageClass name")
 	flag.StringVar(&framework.DockerRegistry, "docker-registry", framework.DockerRegistry, "User provided docker repository")
 	flag.StringVar(&framework.DBCatalogName, "db-catalog", framework.DBCatalogName, "Postgres version")
@@ -64,10 +75,8 @@ func TestE2e(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	// Kubernetes config
-	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube/config")
 	By("Using kubeconfig from " + kubeconfigPath)
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	config, err := clientcmd.BuildConfigFromContext(kubeconfigPath, kubeContext)
 	Expect(err).NotTo(HaveOccurred())
 	// raise throttling time. ref: https://github.com/appscode/voyager/issues/640
 	config.Burst = 100
