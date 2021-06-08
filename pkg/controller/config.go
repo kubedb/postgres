faecf01df41a8bd81b9c3537d424775682776d59
Tamal Saha <tamal@appscode.com>
2021-06-08 01:37:04 -0700

    Create auditor if license file is provided (#503)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/controller/config.go
+++ pkg/controller/config.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/metadata"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
 	reg_util "kmodules.xyz/client-go/admissionregistration/v1beta1"
 	core_util "kmodules.xyz/client-go/core/v1"
@@ -71,15 +72,21 @@ func (c *OperatorConfig) New() (*Controller, error) {
 		return nil, err
 	}
 
-	// audit event publisher
-	natscfg, err := auditlib.NewNatsConfig(c.KubeClient.CoreV1().Namespaces(), c.LicenseFile)
-	if err != nil {
-		return nil, err
-	}
 	mapper := discovery.NewResourceMapper(discovery.NewRestMapper(c.KubeClient.Discovery()))
-	fn := auditlib.BillingEventCreator{
-		Mapper:    mapper,
-		LicenseID: natscfg.LicenseID,
+
+	// audit event publisher
+	// WARNING: https://stackoverflow.com/a/46275411/244009
+	var auditor cache.ResourceEventHandler
+	if c.LicenseFile != "" {
+		natscfg, err := auditlib.NewNatsConfig(c.KubeClient.CoreV1().Namespaces(), c.LicenseFile)
+		if err != nil {
+			return nil, err
+		}
+		fn := auditlib.BillingEventCreator{
+			Mapper:    mapper,
+			LicenseID: natscfg.LicenseID,
+		}
+		auditor = auditlib.NewEventPublisher(natscfg, mapper, fn.CreateEvent)
 	}
 
 	ctrl := New(
@@ -94,7 +101,7 @@ func (c *OperatorConfig) New() (*Controller, error) {
 		topology,
 		c.Recorder,
 		mapper,
-		auditlib.NewEventPublisher(natscfg, mapper, fn.CreateEvent),
+		auditor,
 	)
 
 	if err := ctrl.EnsureCustomResourceDefinitions(); err != nil {
